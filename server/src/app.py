467f27e78907e33853d3ea3c63cc01d21db1e06d
Yoel <ybencab@gmail.com>
2024-07-18 16:05:23 +0100

    getting files

--- server/src/app.py
+++ server/src/app.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-from routes import content
+import os
 
 app = Flask(__name__)
+
+STORAGE_DIRECTORY = "/home/data"
 
 @app.route("/")
 def hello_world():
   return "CloudAtHome API"
 
 @app.route("/content")
-def conten():
-  return content.test()
+def list_content():
+  return os.listdir("/home/data")
+
+@app.route("/get-file/<string:filename>")
+def get_file(filename):
+  file_path = os.path.join(STORAGE_DIRECTORY, filename)
+  if not os.path.exists(file_path):
+    return "404 File Not Found", 404
+  
+  try:
+    return send_from_directory(STORAGE_DIRECTORY, filename, as_attachment=True)
+  except Exception as e:
+    return f"An error ocurred: {str(e)}", 500
 
 if __name__ == "__main__":
     app.run()
--- server/src/routes/content.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def test():
-  contenido = os.listdir("/home/data")
-  return f"it works!, {contenido}"
